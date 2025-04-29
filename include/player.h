d4805974f7c70e006f6fcc5a0ff4e633806f5532
Annabelle <annabelle.goter@eleves.ec-nantes.fr>
2025-04-29 15:10:42 +0200

    classe player by Annabelle

--- include/player.h
+++ include/player.h
@@ -3,28 +3,27 @@
 
 #include <string>
 
-class Player {
-protected:
- std::string nom ;
- int prediction ;
- int score ;
+class Player
+{
+private:
+    std::string name;
+    int prediction;
+    int score;
 
 public:
+    /* Constructors */
+    Player(std::string name);
 
-/* Constructors */
- Player(std::string nom);
+    /* Getters */
+    std ::string getName();
+    int getPrediction();
+    int getScore();
 
- /* Getters */
- std :: string getName();
- int getPrediction();
- int getScore();
+    /* Setters */
+    void setPrediction(int prediction);
 
-/* Setters */
-void setPrediction(int prediction);
-
-/* Methods */
-void incrementScore (int score);
-
-} ;
+    /* Methods */
+    void incrementScore(int score);
+};
 
 #endif // PLAYER_H
