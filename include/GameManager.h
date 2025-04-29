6941cf3aa1a9be301092a7873ade66f358febe96
Damien Bureau <damien.bureau14@gmail.com>
2025-04-29 16:49:30 +0200

    [Dev] Add GameManager header

---
+++ include/GameManager.h
@@ -0,0 +1,38 @@
+#ifndef GAME_MANAGER_H
+#define GAME_MANAGER_H
+
+#include <vector>
+
+#include "typedef.h"
+#include "Tile.h"
+#include "Board.h"
+#include "Player.h"
+
+class GameManager
+{
+private:
+    Tile goal_tile;
+    Board board;
+    std::vector<Player> players;
+
+public:
+    /* Constructors */
+    GameManager();
+
+    /* Getters */
+    Tile getGoalTile();
+    std::vector<Player> getPlayers();
+    Player getPlayer(int index);
+
+    /* Methods */
+    void addPlayer(Player player);
+    void removePlayer(Player player);
+    void displayBoard();
+    void setupRound();
+    void processPredictionsInputs();
+    void sortPlayersByPredictions();
+    void playRound();
+    void displayResults();
+};
+
+#endif /* GAME_MANAGER_H */
