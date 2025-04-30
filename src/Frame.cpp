b365de8ee40da0db459f2b1a0ef12e1788313563
Thomas Verron <verronthomas03@gmail.com>
2025-04-30 10:59:25 +0200

    [ReviewFix] Add Tile + replace wall by walls

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -3,12 +3,12 @@
 Frame::Frame()
     : tile(nullptr), walls{false, false, false, false} {}
 
-Frame::Frame(Tile *t, bool wall[4])
+Frame::Frame(Tile *t, bool walls[4])
     : tile(t)
 {
     for (size_t i = 0; i < 4; ++i)
     {
-        this->walls[i] = wall[i];
+        this->walls[i] = walls[i];
     }
 }
 
