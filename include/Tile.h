2df110c92a4991d02a4fa1e3ede39635b8b36b61
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-04-30 11:58:38 +0200

    [Dev] Add Tile::getEmojiFromTile method

--- include/Tile.h
+++ include/Tile.h
@@ -1,15 +1,62 @@
 #pragma once 
 
+#include <string>
+
 #include "typedef.h"
 
 
 class Tile
 {
-    private:
-        Color color;
-        Shape shape;
-    public:
-        Tile(Color color, Shape shape) : color(color), shape(shape) {}
-        Color getColor() const { return color; }
-        Shape getShape() const { return shape; }
+private:
+    Color color;
+    Shape shape;
+
+public:
+    Tile(Color color, Shape shape) : color(color), shape(shape) {}
+    Color getColor() const { return color; }
+    Shape getShape() const { return shape; }
+    static const std::string& getEmojiFromTile(const Tile& tile)
+    {
+        std::string emoji;
+        std::string shape_char;
+        switch (tile.shape)
+        {
+        case Shape::CIRCLE:
+            shape_char = "●";
+            break;
+        case Shape::SQUARE:
+            shape_char = "◼";
+            break;
+        case Shape::TRIANGLE:
+            shape_char = "▲";
+            break;
+        case Shape::STAR:
+            shape_char = "🟊";
+            break;
+        default:
+            break;
+        }
+
+        switch (tile.color)
+        {
+        case Color::RED:
+            emoji = "\033[31m" + shape_char + "\033[0m"; // Red
+            break;
+        case Color::GREEN:
+            emoji = "\033[32m" + shape_char + "\033[0m"; // Green
+            break;
+        case Color::BLUE:
+            emoji = "\033[34m" + shape_char + "\033[0m"; // Blue
+            break;
+        case Color::YELLOW:
+            emoji = "\033[33m" + shape_char + "\033[0m"; // Yellow
+            break;
+        case Color::RAINBOW:
+            emoji = "🌈"; 
+            break;
+        default:
+            break;
+        }
+        return emoji;
+    }
 };
