85508c37e695d49db889c28439aac5b885d496dd
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-05-05 08:17:41 +0200

    [Dev-Update] Update Board Generation

--- src/Board.cpp
+++ src/Board.cpp
@@ -5,9 +5,13 @@
 
 /* Constructors */
 
-Board::Board() : quarterBoards{{QuarterBoard(0), QuarterBoard(1)},
-                               {QuarterBoard(2), QuarterBoard(3)}}
+Board::Board()
+: quarterBoards{{QuarterBoard(0), QuarterBoard(1)}, {QuarterBoard(2), QuarterBoard(3)}}
 {
+    this->quarterBoards[0][0] = this->generateQuarterBoard(true, true);
+    this->quarterBoards[0][1] = this->generateQuarterBoard(true, false);
+    this->quarterBoards[1][0] = this->generateQuarterBoard(false, true);
+    this->quarterBoards[1][1] = this->generateQuarterBoard(false, false);
 }
 
 /* Getters */
@@ -22,28 +26,37 @@ Frame Board::getFrame(int x, int y)
 
 /* Methods */
 
+QuarterBoard Board::generateQuarterBoard(bool top, bool left)
+{
+
+}
+
 void Board::generate()
 {
-    /* Random shuffle the quarterboards */
-    std::vector<QuarterBoard> quarterboards = {this->quarterBoards[0][0],
-                                               this->quarterBoards[0][1],
-                                               this->quarterBoards[1][0],
-                                               this->quarterBoards[1][1]};
-    std::random_shuffle(quarterboards.begin(), quarterboards.end());
-    this->quarterBoards[0][0] = quarterboards[0];
-    this->quarterBoards[0][1] = quarterboards[1];
-    this->quarterBoards[1][0] = quarterboards[2];
-    this->quarterBoards[1][1] = quarterboards[3];
+    // /* Random shuffle the quarterboards */
+    // std::vector<QuarterBoard> quarterboards = {this->quarterBoards[0][0],
+    //                                            this->quarterBoards[0][1],
+    //                                            this->quarterBoards[1][0],
+    //                                            this->quarterBoards[1][1]};
+    // std::random_shuffle(quarterboards.begin(), quarterboards.end());
+    // this->quarterBoards[0][0] = quarterboards[0];
+    // this->quarterBoards[0][1] = quarterboards[1];
+    // this->quarterBoards[1][0] = quarterboards[2];
+    // this->quarterBoards[1][1] = quarterboards[3];
 
-    /* Randomly apply symetry to the quarterboards */
-    for (int x = 0; x < 2; x++)
-    {
-        for (int y = 0; y < 2; y++)
-        {
-            if (rand() % 2 == 0)
-            {
-                this->quarterBoards[x][y] = this->quarterBoards[x][y].ApplySymmetry();
-            }
-        }
-    }
+    // /* Randomly apply symetry to the quarterboards */
+    // for (int x = 0; x < 2; x++)
+    // {
+    //     for (int y = 0; y < 2; y++)
+    //     {
+    //         if (rand() % 2 == 0)
+    //         {
+    //             this->quarterBoards[x][y] = this->quarterBoards[x][y].ApplySymmetry();
+    //         }
+    //     }
+    // }
+    this->quarterBoards[0][0] = this->generateQuarterBoard(true, true);
+    this->quarterBoards[0][1] = this->generateQuarterBoard(true, false);
+    this->quarterBoards[1][0] = this->generateQuarterBoard(false, true);
+    this->quarterBoards[1][1] = this->generateQuarterBoard(false, false);
 }
