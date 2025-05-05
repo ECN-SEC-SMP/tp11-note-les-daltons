3d5dae7b1c92cc5e0689379fb9f090044bedc8a4
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2025-05-05 13:00:13 +0200

    [Doc] GameManager Documentation

--- include/GameManager.h
+++ include/GameManager.h
@@ -7,6 +7,7 @@
 #include "Tile.h"
 #include "Board.h"
 #include "Player.h"
+#include "Robot.h"
 
 class GameManager
 {
@@ -14,6 +15,7 @@ private:
     Tile* goal_tile;
     Board board;
     std::vector<Player*> players;
+    std::vector<Robot> robots;
 
 public:
     /* Constructors */
@@ -25,13 +27,48 @@ public:
     Player* getPlayer(int index);
 
     /* Methods */
+    /**
+     * @brief Adds a player to the game.
+     * 
+     * @param player (IN) Pointer to the player to be added.
+     */
     void addPlayer(Player* player);
+    /**
+     * @brief Removes a player from the game.
+     * 
+     * @param player (IN) Pointer to the player to be removed.
+     */
     void removePlayer(Player* player);
-    void displayBoard();
+    /**
+     * @brief Returns displayed board string
+     * 
+     * @return std::string 
+     */
+    std::string displayBoard();
+    /**
+     * @brief Sets up the game for a new round.
+     * @note This function is called at the beginning of each round.
+     */
     void setupRound();
+    /**
+     * @brief Processes the inputs for the predictions of each player.
+     * @note This function is called when the players have to made their predictions.
+     */
     void processPredictionsInputs();
+    /**
+     * @brief Sorts the players based on their predictions.
+     * @note This function is called after the players have made their predictions.
+     */
     void sortPlayersByPredictions();
+    /**
+     * @brief Plays a round of the game.
+     * @note This function is called to play a round of the game.
+     */
     void playRound();
+    /**
+     * @brief Displays the results of the game.
+     * @note This function is called at the end of the game.
+     */
     void displayResults();
 };
 
